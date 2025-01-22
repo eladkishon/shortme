c116281b16206c6e952ba53952a66f693b3c6481
Elad Kishon <elad.kishon@gmail.com>
2025-01-21 22:30:14 -0600

    docs

--- src/lib/utils/base62.ts
+++ src/lib/utils/base62.ts
@@ -1,6 +1,11 @@
 const CHARSET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 const BASE = CHARSET.length;
 
+/**
+ * Encode a number to a base62 string
+ * @param num - The number to encode
+ * @returns The encoded string
+ */
 function encode(num: number): string {
   if (num === 0) return CHARSET[0];
   
@@ -12,6 +17,11 @@ function encode(num: number): string {
   return encoded;
 }
 
+/**
+ * Decode a base62 string to a number
+ * @param str - The base62 string to decode
+ * @returns The decoded number
+ */
 function decode(str: string): number {
   return str.split('').reverse().reduce((result, char, index) => {
     return result + CHARSET.indexOf(char) * Math.pow(BASE, index);
